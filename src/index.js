e59a9238dffb869e2c509b6346befcc07264b771
Theodore Wahle <theodorewahle@gmail.com>
2019-04-25 01:46:02 -0400

    Starter pack

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.scss';
+
+const App = () => <div className="test">All the REACT are belong to us!</div>;
+
+ReactDOM.render(<App />, document.getElementById('main'));
